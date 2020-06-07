61cf3f2a6f3f9951052e338e5842d9b7f23925c3
javierOnt <javieront2000@gmail.com>
2020-06-06 23:51:58 -0500

    directory reducer added, explore page now display with redux and not with state

--- src/redux/redux-reducers.js
+++ src/redux/redux-reducers.js
@@ -1,11 +1,21 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user-reducer";
 import cartReducer from "./cart-reducer";
+import directoryReducer from "./directory-reducer";
 
-const allReducers = combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
-export default allReducers;
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import allReducers from "./redux-reducers";
 
 const middlewares = [logger];
 
-const store = createStore(allReducers, applyMiddleware(middlewares[0]));
-
-export default store;
+export const store = createStore(allReducers, applyMiddleware(middlewares[0]));
+export const persistor = persistStore(store);
